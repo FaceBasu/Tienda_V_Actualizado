6edf628e523048c90364fc0a26ae36db70d03289
abrahamduraan <abrahamfduran@gmail.com>
2022-10-08 12:57:38 -0600

    Semana 4, Uso de MySql inicial

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,36 @@
+package com.tienda.controller;
+
+import com.tienda.dao.ClienteDao;
+import com.tienda.dominio.Cliente;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    //Provoca que el objeto se cree si no existe o se use el que ya existe... no hace mas de 1 objeto
+    //Esto se conoce como inyeccion de dependencias...
+    @Autowired
+    private ClienteDao clienteDao;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var texto = "Estamos en semana 4";
+        model.addAttribute("saludo", texto);
+
+        /*Cliente cliente1 = new Cliente("Pedro", "Jimenez Retana", "pjimenez@gmail.com", "88888888");
+        Cliente cliente2 = new Cliente("Juan", "Retana Jimenez", "pretana@gmail.com", "88888887");
+        Cliente cliente3 = new Cliente("Pablo", "Jerez Rodriguez", "pjerez@gmail.com", "88888886");
+        var clientes=Arrays.asList(cliente1, cliente2, cliente3);*/
+        
+        var clientes = clienteDao.findAll();
+
+        model.addAttribute("clientes", clientes);
+
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/tienda/dao/ClienteDao.java
@@ -0,0 +1,10 @@
+
+package com.tienda.dao;
+
+import com.tienda.dominio.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteDao extends JpaRepository<Cliente, Long> {
+    
+    
+}
